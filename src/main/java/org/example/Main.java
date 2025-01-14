cc05ac1bd5f2157f1d54e0e6ba537d4dc4b124f2
Murat Okçu <mrtkcu0101@gmail.com>
2025-01-14 21:06:11 +0300

    a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.PaintColor;
+
+import static org.example.model.enums.LampType.LAVA;
+import static org.example.model.enums.PaintColor.RED;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom = new Bedroom("sovyet apartman odası",new Wall("bir"),new Wall("iki"),new Wall("üç"),new Wall("dört"),new Ceiling(5,RED),new Bed("sert", 2, 3,4,2),new Lamp(LAVA,true,15), new Wardrobe(3,3,50),new Carpet(1,1,RED));
+
     }
 }
